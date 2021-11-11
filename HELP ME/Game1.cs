6984fd7301a456944f9ea4ac492fa4ce30ec7cbd
Avi ISHAAYA <meishaaya@yahoo.com>
2021-11-11 15:44:28 -0800

    Add project files.

---
+++ HELP ME/Game1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+using System.Collections.Generic;
+
+namespace HELP_ME
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Random random = new Random();
+        char[] answer = "Karan".ToCharArray();
+        char[] letters = "jfnhe".ToCharArray();
+
+        public void Mutate(char[] letters)
+        {
+            for (int i= 0; i < letters.Length; i++)
+            {
+                var letter = letters[i];
+                letter += (char)random.Next(-1, 2);
+            }            
+        }
+
+        public bool Error (char[] newList, char[] oldList, char[] goal, ref bool better)
+        {
+            int newDist = 0;
+            int oldDist = 0;                        
+            for (int i = 0; i < newList.Length; i++)
+            {
+                newDist += (int)MathHelper.Distance(newList[i], goal[i]);
+                oldDist += (int)MathHelper.Distance(oldList[i], goal[i]);
+            }
+            better = newDist < oldDist;
+            return newDist == 0;            
+        }
+       
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
