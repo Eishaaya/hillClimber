cd8fa4cdf567721172f8d32d7e19c48f6577b964
Avi ISHAAYA <meishaaya@yahoo.com>
2021-11-15 12:53:13 -0800

    uint useless piece of shit

--- HELP ME/Game1.cs
+++ HELP ME/Game1.cs
@@ -17,30 +17,91 @@ namespace HELP_ME
         Random random = new Random();
         char[] answer = "Karan".ToCharArray();
         char[] letters = "jfnhe".ToCharArray();
+        bool done = true;     
 
-        public void Mutate(char[] letters)
+        Sprite goal;
+        Sprite climber;
+
+        Texture2D climbingTexture;
+        Texture2D goalTexture;
+
+        uint[] tempList;
+        Color[] climbingColors;
+        Color[] goalColors;
+
+        public void Mutate (Color[] colors)
         {
-            for (int i= 0; i < letters.Length; i++)
-            {
-                var letter = letters[i];
-                letter += (char)random.Next(-1, 2);
-            }            
+            var tempArray = getInts(colors);
+            Mutate(tempArray);
+            colors = GetColors(tempArray);
+        }
+        public bool Error (Color[] newA, uint[] oldA, Color[] goal, ref bool gud)
+        {
+            var tempA = getInts(newA);
+            var tempG = getInts(goal);
+
+            return Error(tempA, oldA, tempG, ref gud);
         }
 
-        public bool Error (char[] newList, char[] oldList, char[] goal, ref bool better)
+        public uint[] getInts(Color[] colors)
         {
-            int newDist = 0;
-            int oldDist = 0;                        
+            var tempArray = new uint[colors.Length];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                tempArray[i] = colors[i].PackedValue;
+            }
+            return tempArray;
+        }
+
+        public Color[] GetColors(uint[] tempArray)
+        {
+            var colors = new Color[tempArray.Length];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                colors[i] = new Color((uint)tempArray[i]);
+            }
+            return colors;
+        }
+
+        public void Mutate(uint[] letters)
+        {
+            var i = random.Next(0, letters.Length);
+            letters[i] += (uint)(random.Next(0, 2) == 0 ? 1 : -1);
+        }
+
+        public static string BeYarn(char[] thread)
+        {
+            string quilt = "";
+            foreach (var spool in thread)
+            {
+                quilt += spool;
+            }
+
+            return quilt;
+        }
+        public char[] Shuffle(int length)
+        {
+            var returner = new char[length];
+            for (int i = 0; i < returner.Length; i++)
+            {
+                returner[i] = (char)random.Next(41, 123);
+            }
+            return returner;
+        }
+        public bool Error(uint[] newList, uint[] oldList, uint[] goal, ref bool better)
+        {
+            uint newDist = 0;
+            uint oldDist = 0;
             for (int i = 0; i < newList.Length; i++)
             {
-                newDist += (int)MathHelper.Distance(newList[i], goal[i]);
-                oldDist += (int)MathHelper.Distance(oldList[i], goal[i]);
+                newDist += (uint)MathHelper.Distance(newList[i], goal[i]);
+                oldDist += (uint)MathHelper.Distance(oldList[i], goal[i]);
             }
             better = newDist < oldDist;
-            return newDist == 0;            
+            return newDist == 0;
         }
-       
-        
+
+
 
         public Game1()
         {
@@ -56,8 +117,9 @@ namespace HELP_ME
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -67,10 +129,37 @@ namespace HELP_ME
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+                        
+            int width = 2;
+            int height = 2;
+            climbingTexture = new Texture2D(GraphicsDevice, width, height);
+            goalTexture = new Texture2D(GraphicsDevice, width, height);
 
-            // TODO: use this.Content to load your game content here
+            var pixelColor = new Color[width * height];
+            for (int i = 0; i < pixelColor.Length; i++)
+            {
+                pixelColor[i] = Color.White;                
+            }
+            climbingTexture.SetData(pixelColor);
+
+            var fuckOff = new Color[width * height];
+            for (int i = 0; i < fuckOff.Length; i++)
+            {
+                fuckOff[i] = Color.Blue;
+            }
+
+            goalTexture.SetData(fuckOff);
+
+            climber = new Sprite(climbingTexture, new Vector2(0, 0));
+            climber.Scale = 500 / width;
+            goal = new Sprite(goalTexture, new Vector2(width * climber.Scale, 0));
+            goal.Scale = climber.Scale;
+
+            climbingColors = pixelColor;
+            goalColors = new Color[pixelColor.Length];
+            goalTexture.GetData(goalColors);
+            tempList = getInts(climbingColors);
         }
 
         /// <summary>
@@ -89,7 +178,53 @@ namespace HELP_ME
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            
+            //if (!done)
+            //{
+            //    var tempList = (char[])letters.Clone();
+            //    Mutate(ref letters);
+            //    bool gud = false;
+
+            //    if (Error(letters, tempList, answer, ref gud))
+            //    {
+            //        Console.WriteLine($"Reached goal of {BeYarn(answer)}");
+            //        done = true;
+            //    }
+            //    if (gud)
+            //    {
+            //        Console.WriteLine($"Positive mutation, {BeYarn(letters)}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Negative mutation, {BeYarn(letters)} reverted back to {BeYarn(tempList)}");
+            //        letters = tempList;
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter word(s):");
+            //    string words = Console.ReadLine();
+            //    answer = words.ToCharArray();
+            //    letters = Shuffle(answer.Length);
+            //    done = false;
+            //}
+
+            bool gud = false;
+
+            if (Error(climbingColors, tempList, goalColors, ref gud))
+            {
+                done = true;
+            }
+            if (!gud)
+            {
+                climbingColors = GetColors(tempList);
+            }
+
+            tempList = getInts(climbingColors);
+            Mutate(climbingColors);
+
+            climber.Image.SetData(climbingColors);
+            goal.Image.SetData(goalColors);
 
             base.Update(gameTime);
         }
@@ -101,9 +236,10 @@ namespace HELP_ME
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            goal.Draw(spriteBatch);
+            climber.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ HELP ME/VisualObject.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HELP_ME
+{
+    public class VisualObject
+    {
+        public Color Color;
+        public Vector2 Location;
+        public float rotation;
+        public float Scale;
+        public float Depth;
+        public SpriteEffects effect;
+        public Vector2 Origin;
+        protected bool moved;
+
+        protected bool bigger;
+        protected bool rotated;
+        protected Vector2 spotSet;
+        protected float sizeSet;
+        protected float degreeSet;
+        public Vector2 offset;
+        protected float oldScale;
+        protected Random random;
+        public Color originalColor;
+        public float oldRotation;
+
+        public VisualObject(Vector2 location, Color color, Vector2 origin, float Rotation, SpriteEffects Effect, float scale, float depth)
+        {
+            Location = location;
+            Color = color;
+            Origin = origin;
+            Scale = scale;
+            Depth = depth;
+            effect = Effect;
+            rotation = Rotation;
+            originalColor = color;
+            oldScale = Scale;
+            oldRotation = rotation;
+            random = Extensions.random;
+
+            offset = Vector2.Zero;
+            moved = false;
+            bigger = false;
+            sizeSet = float.NaN;
+            degreeSet = float.NaN;
+            spotSet = new Vector2(float.NaN, float.NaN);
+        }
+
+        #region clone
+
+        public VisualObject Clone()
+        {
+            var copy = new VisualObject(Location, Color, Origin, rotation, effect, Scale, Depth);
+            CloneLogic(copy);
+            return copy;
+        }
+        protected void CloneLogic<T>(T copy) where T:VisualObject
+        {
+            copy.bigger = bigger;
+            copy.rotated = rotated;
+            copy.spotSet = spotSet;
+            copy.sizeSet = sizeSet;
+            copy.degreeSet = degreeSet;
+            copy.offset = offset;
+            copy.oldScale = oldScale;
+            copy.originalColor = originalColor;
+            copy.oldRotation = oldRotation;
+        }
+
+
+        #endregion
+
+        #region visualFunctions
+        public void Vibrate(int distance, float sped, bool rando = true)
+        {
+            if (float.IsNaN(spotSet.X))
+            {
+                if (rando)
+                {
+                    spotSet = new Vector2(random.Next(-distance, distance), random.Next(-distance, distance));
+                }
+                else
+                {
+                    spotSet = new Vector2(distance, distance);
+                }
+                offset = Vector2.Zero;
+            }
+            if (!moved)
+            {
+                offset = Vector2.Lerp(offset, spotSet, sped);
+                if (Vector2.Distance(offset, spotSet) <= .5f)
+                {
+                    offset = spotSet;
+                    moved = true;
+                }
+            }
+            else
+            {
+                offset = Vector2.Lerp(offset, Vector2.Zero, sped);
+                if (Vector2.Distance(offset, Vector2.Zero) <= .5f)
+                {
+                    spotSet = new Vector2(float.NaN, float.NaN);
+                    moved = false;
+                    offset = Vector2.Zero;
+                }
+            }
+        }
+        public void Pulsate(int size, float sped, bool rando = true)
+        {
+            if (float.IsNaN(sizeSet))
+            {
+                if (rando)
+                {
+                    sizeSet = random.Next(size) + oldScale * 100;
+                }
+                else
+                {
+                    sizeSet = size;
+                }
+                sizeSet /= 100;
+            }
+            if (!bigger)
+            {
+                var temp = Vector2.Lerp(new Vector2(Scale, 0), new Vector2(sizeSet, 0), sped);
+                if (Vector2.Distance(temp, new Vector2(sizeSet, 0)) <= .01f)
+                {
+                    Scale = sizeSet;
+                    bigger = true;
+                }
+                else
+                {
+                    Scale = temp.X;
+                }
+            }
+            else
+            {
+                var temp = Vector2.Lerp(new Vector2(Scale, 0), new Vector2(oldScale, 0), sped);
+                if (Vector2.Distance(temp, new Vector2(oldScale, 0)) <= .01f)
+                {
+                    sizeSet = float.NaN;
+                    Scale = oldScale;
+                    bigger = false;
+                }
+                else
+                {
+                    Scale = temp.X;
+                }
+            }
+        }
+        public void Rotate(float target, float sped, bool rando = true)
+        {
+            if (float.IsNaN(degreeSet))
+            {
+                if (rando)
+                {
+                    degreeSet = random.Next((int)-target * 1000, (int)target * 1000);
+                    degreeSet /= 1000;
+                }
+                else
+                {
+                    degreeSet = target;
+                }
+            }
+            if (!rotated)
+            {
+                var temp = MathHelper.Lerp(rotation, oldRotation, sped);
+                if (MathHelper.Distance(temp, oldRotation) <= .01f)
+                {
+                    rotation = degreeSet;
+                    rotated = true;
+                }
+                else
+                {
+                    rotation = temp;
+                }
+            }
+            else
+            {
+                var temp = Vector2.Lerp(new Vector2(rotation, 0), new Vector2(degreeSet, 0), sped);
+                if (Vector2.Distance(temp, new Vector2(rotation, 0)) <= .01f)
+                {
+                    degreeSet = float.NaN;
+                    rotation = temp.X;
+                    rotated = false;
+                }
+                else
+                {
+                    rotation = temp.X;
+                }
+            }
+        }
+        public void ChangeColor(Color newColor, float sped = .1f)
+        {
+            var temp = Color;
+            Color = Color.FromNonPremultiplied((int)MathHelper.LerpPrecise(Color.R, newColor.R, sped), (int)MathHelper.LerpPrecise(Color.G, newColor.G, sped), (int)MathHelper.LerpPrecise(Color.B, newColor.B, sped), (int)MathHelper.LerpPrecise(Color.A, newColor.A, sped));
+            if (temp == Color)
+            {
+                Color = newColor;
+            }
+        }
+        public bool Fade(int speed = 3)
+        {
+            return Fade(originalColor, speed);
+        }
+        public bool Fade(Color tint, int speed = 3)
+        {
+            if (Color.A <= 0)
+            {
+                Color = tint;
+                Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, 0);
+                return true;
+            }
+            Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, Color.A - speed);
+            return false;
+        }
+
+        public bool FadeTo(int cChoice)
+        {
+            var tint = Color;
+            if (cChoice == 0)
+            {
+                tint = Color.FromNonPremultiplied(Color.R - Color.A, Color.G - Color.A, Color.B - Color.A, Color.A);
+            }
+            else if (cChoice == 1)
+            {
+                tint = Color.FromNonPremultiplied(Color.A, Color.G - Color.A, Color.B - Color.A, Color.A);
+            }
+            else if (cChoice == 2)
+            {
+                tint = Color.FromNonPremultiplied(Color.R - Color.A, Color.A, Color.B - Color.A, Color.A);
+            }
+            else if (cChoice == 3)
+            {
+                tint = Color.FromNonPremultiplied(Color.R - Color.A, Color.G - Color.A, Color.A, Color.A);
+            }
+            if (Color.A <= 0)
+            {
+                Color = tint;
+                Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, 0);
+                return true;
+            }
+            Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, Color.A - 3);
+            return false;
+        }
+        public bool Fill()
+        {
+            return Fill(originalColor);
+        }
+        public bool Fill(Color tint)
+        {
+            Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, Color.A + 3);
+            if (Color.A >= 255)
+            {
+                Color = Color.FromNonPremultiplied(tint.R, tint.G, tint.B, 255);
+                return true;
+            }
+            return false;
+        }
+
+
+        #endregion
+    }
+}
